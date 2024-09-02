687b61531dab1a822f54ccf942f0e5ecb397cf1a
ARTiKZ <145798465+ART1KZ@users.noreply.github.com>
2024-09-02 19:02:51 +0500

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,25 +1,23 @@
 @echo off
 :menu
-chcp 65001
 cls
 
-echo Привет, это скрипт для блума! Автор скрипта - https://funpay.com/users/3305353/. Ниже представлено меню
-@echo.
 echo Hi, this is a script for blum! The author of the script is https://funpay.com/users/3305353/. Below is the menu
 @echo.
 
 echo =================== Menu =====================================
-echo 1. Запустить скрипт с заморозками / Run script with freezes
-echo 2. Запустить скрипт без заморозок / Run script without freezes
-echo 3. Выход / Exit
+echo 1. Run script with freezes
+echo 2. Run script without freezes
+echo 3. Exit
 echo ==============================================================
-set /p choice=Введите число / Enter a number (1-3): 
+set /p choice=Enter a number (1-3): 
 
 if "%choice%"=="1" goto script1
 if "%choice%"=="2" goto script2
 if "%choice%"=="3" goto exit
-echo Вы ввели неверное числоInvalid choice
+echo Invalid choice
 goto menu
+
 
 :script1
 cd script
