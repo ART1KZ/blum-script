5ad5ad0fe5f1aa8e671c5e7bd45e52d1ade5b2a4
ARTiKZ <145798465+ART1KZ@users.noreply.github.com>
2024-12-24 14:37:25 +0500

    Update run.bat

--- run.bat
+++ run.bat
@@ -9,7 +9,7 @@ echo =================== Menu =====================================
 echo 1. Run script
 echo 2. Exit
 echo ==============================================================
-set /p choice=Enter a number (1-3): 
+set /p choice=Enter a number (1-2): 
 
 if "%choice%"=="1" goto script
 if "%choice%"=="2" goto exit
